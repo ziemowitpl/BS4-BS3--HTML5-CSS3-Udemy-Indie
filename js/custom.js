d61d68cdab1c4a56eef2ddf38c3e1d0e097dbb41
ziemowitpl <roninrallegro@gmail.com>
2018-04-13 22:18:13 +0200

    Add gallery effect (magnific-popup) and personalize.

--- js/custom.js
+++ js/custom.js
@@ -5,3 +5,16 @@ $(function() {
     // animated on scroll
     new WOW().init();
 });
+// ==========================================
+//                 SERVICES
+// ==========================================
+$(document).ready(function() {
+    $('#work').magnificPopup({
+        delegate: 'a', // child items selector, by clicking on it popup will open
+        type: 'image',
+        // other options
+        gallery:{
+            enabled:true
+          }
+    });
+  });
