32b51020bbc7693b8d73b8454b47d8d89a1d2217
ziemowitpl <roninrallegro@gmail.com>
2018-04-16 10:56:22 +0200

    Add class and code of carousel OWL to Testimonials.

--- js/custom.js
+++ js/custom.js
@@ -33,3 +33,16 @@ $(document).ready(function() {
         autoplayHoverPause: true
     });
   });
+// ==========================================
+//                 TESTIMONIALS
+// ==========================================
+
+  $(document).ready(function() {
+    $("#customers-testimonials").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+  });
